9f6c5bb702ded42679170470a768daca32037efb
Komal-Arista <komal@aristasystems.in>
2024-08-08 15:54:50 +0530

    updated code

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    {name:'Komal nagdev', email:'komalnagdev@gmail.com'},
+    {name:'Komal Ramrakhyani', email:'komalramrakhyani@gmail.com'},
+    {name:'Komal Sanai', email:'komalsanai@gmail.com'},
+];
+module.exports = data;
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("test");
+// var colors = require('colors');
+
+// console.log("test purpose1111 test 123".magenta);
+
+const http = require('http');
+const data = require('./data');
+
+http.createServer((req,resp) => {
+    resp.writeHead(200, {'Content-Type':'application\json'});
+    resp.write(JSON.stringify(data));
+    resp.end();
+}).listen(5000);
